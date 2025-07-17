c5a7bf3568846b1aa2f1b650db4e7c12b17ff555
Errol13 <erroljohncelis13@gmail.com>
2025-07-17 12:38:17 +0800

    positive and negative testing added

--- test_swaglab.py
+++ test_swaglab.py
@@ -5,39 +5,80 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_swaglab_login():
-    #declare the browser to be used or driver
-    driver = webdriver.Chrome()
+
+#helpfer function for repeatable login steps
+def perform_login(driver, username, password):
+    #visit the site
     driver.get("https://www.saucedemo.com/")
+    print("Performing login...")
+
+    #find the element for the username
+    username_input= driver.find_element(By.ID, "user-name")
+    username_input.clear()
+    username_input.send_keys(username)
+
+
+    #also find the element of the password form
+    password_input = driver.find_element(By.ID, "password")
+    password_input.clear()
+    password_input.send_keys(password)
+
+
+    #submit
+    submit_button = driver.find_element(By.ID, "login-button")
+    submit_button.click()
+
+
+#create parameters for different valid credentials 
+@pytest.mark.parametrize("username",[
+    ("standard_user"),
+    ("visual_user"),
+    ("performance_glitch_user")
+])
+def test_swaglab_valid_credentials(username):
+    driver = webdriver.Chrome()
+    
     try:
-        #get the title and print it
-        print(driver.title)
-        assert "Swag Labs" in driver.title
-
-
-        #ind the element for the username
-        username_input= driver.find_element(By.ID, "user-name")
-        username_input.clear()
-        username_input.send_keys("standard_user")
-
-
-        #also find the element of the password form
-        password_input = driver.find_element(By.ID, "password")
-        password_input.clear()
-        password_input.send_keys("secret_sauce")
-
-
-        #submit
-        submit_button = driver.find_element(By.ID, "login-button")
-        submit_button.click()
-
-        # Wait a bit to let page load
-        WebDriverWait(driver, 10).until(
-            EC.url_contains("inventory.html")
+        # call the perform login
+        perform_login(driver, username, "secret_sauce")
+        
+        #wait until it is successfully logged in
+        WebDriverWait(driver,10).until(
+            EC.url_contains("inventory")
         )
 
-        #Assert that it successfully logged in with inventory in its url
-        assert "inventory.html" in driver.current_url
-
+        #assert if it contains inventory
+        assert "inventory" in driver.current_url
+    
     finally:
-        driver.quit()
+        try:
+            driver.quit()
+        except:
+            print("Browser already closed!, skipping quit")
+
+
+#create parameters for different invalid credentials 
+@pytest.mark.parametrize("username, password",[
+    ("standard_user", "wrong_pass"),
+    ("locked_out_user", "secret_sauce"),
+    ("problem_user", "grr-angrr")
+])
+def test_swaglab_invalid_credentials(username, password):
+    driver = webdriver.Chrome()
+    
+    try:
+        # call the perform login
+        perform_login(driver, username, password)
+        
+        #wait until it is successfully logged in
+        error = WebDriverWait(driver,10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "error-message-container"))
+        )
+        #assert if it contains error message 
+        assert error.is_displayed()
+    
+    finally:
+        try:
+            driver.quit()
+        except:
+            print("Browser was already closed! skipping quit")
