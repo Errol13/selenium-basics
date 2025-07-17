23632bf6fc670d649a03f12d42692050078e4581
Errol13 <erroljohncelis13@gmail.com>
2025-07-17 11:32:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Selenium Basics
+
+This project is a basic introduction to web automation using Selenium. 
+
+## Demonstrated Knowledge
+- Open a browser using Selenium WebDriver
+- Locate and interact with web elements (e.g., input fields, buttons)
+- Automate a login workflow
+- Use Python `assert` statements for validation
+- Structure tests using `pytest` for test automation
---
+++ test_swaglab.py
@@ -0,0 +1,43 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+
+
+def test_swaglab_login():
+    #declare the browser to be used or driver
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    try:
+        #get the title and print it
+        print(driver.title)
+        assert "Swag Labs" in driver.title
+
+
+        #ind the element for the username
+        username_input= driver.find_element(By.ID, "user-name")
+        username_input.clear()
+        username_input.send_keys("standard_user")
+
+
+        #also find the element of the password form
+        password_input = driver.find_element(By.ID, "password")
+        password_input.clear()
+        password_input.send_keys("secret_sauce")
+
+
+        #submit
+        submit_button = driver.find_element(By.ID, "login-button")
+        submit_button.click()
+
+        # Wait a bit to let page load
+        time.sleep(2)
+
+        #Assert that it successfully logged in with inventory in its url
+        assert "inventory.html" in driver.current_url
+
+    finally:
+        driver.quit()
